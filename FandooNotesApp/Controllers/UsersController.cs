f49df90be4513eda6c08081d5f7daabcf8d96315
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-28 15:10:28 +0530

    [vaishnavi] added: presentation layer

---
+++ FandooNotesApp/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+
+namespace FandooNotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly IUserManager manager;
+
+        public UsersController(IUserManager manager)
+        {
+            this.manager = manager;
+        }
+
+        [HttpPost]
+        [Route("Reg")]
+        public IActionResult Register(RegisterModel model)
+        {
+            var checkMail = manager.IsEmailExist(model.Email);
+            if (checkMail)
+            {
+                return BadRequest(new ResponseModel<bool> { Success = false, Message = "Email already exists!", Data = checkMail });
+            }
+            else
+            {
+                var result = manager.Registration(model);
+                if (result != null)
+                {
+                    return Ok(new ResponseModel<Users> { Success = true, Message = "Registration Successful", Data = result });
+                }
+                else
+                {
+                    return BadRequest(new ResponseModel<Users> { Success = true, Message = "Registration Failed" });
+                }
+            }
+           
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult UserLogin(LoginModel model)
+        {
+            var result = manager.Login(model);
+            if (result != null)
+            {
+                return Ok(new ResponseModel<string> { Success = true, Message = "Login Successful", Data = result });
+            }
+            return BadRequest(new ResponseModel<string> { Success = false, Message = "Login Failed" });
+        }
+
+
+
+    }
+}
---
+++ FandooNotesApp/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using ManagerLayer.Interfaces;
+using ManagerLayer.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RepositoryLayer.Context;
+using RepositoryLayer.Interfaces;
+using RepositoryLayer.Services;
+
+namespace FandooNotesApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<FundooDBContext>(a => a.UseSqlServer(Configuration["ConnectionStrings:DBConnection"]));
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserManager, UserManager>();
+            //for swagger
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger();
+
+            // This middleware serves the Swagger documentation UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
