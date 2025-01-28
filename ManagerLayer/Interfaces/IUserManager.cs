8bca2120a908453c86877564ed32f976c231a4a3
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-28 15:11:55 +0530

    [vaishnavi] added: manager layer

---
+++ ManagerLayer/Interfaces/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace ManagerLayer.Interfaces
+{
+    public interface IUserManager
+    {
+        public Users Registration(RegisterModel model);
+        public bool IsEmailExist(string email);
+        public string Login(LoginModel model);
+
+    }
+}
---
+++ ManagerLayer/Services/UserManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+
+namespace ManagerLayer.Services
+{
+    public class UserManager : IUserManager
+    {
+
+        private readonly IUserRepository user;
+        private readonly FundooDBContext context;
+
+        public UserManager(IUserRepository user, FundooDBContext context)
+        {
+            this.user = user;
+            this.context = context;
+        }
+
+        public Users Registration(RegisterModel model)
+        {
+            return user.Registration(model);
+        }
+
+
+        public bool IsEmailExist(string email)
+        {
+            var result = context.Users.FirstOrDefault(x => x.Email == email);
+            if (result !=null )
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string Login(LoginModel model)
+        {
+            return user.Login(model);
+        }
+
+    }
+}
