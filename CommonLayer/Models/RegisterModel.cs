b0f294c7e5b026c0ba078cd51b934438a6750781
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-28 15:09:41 +0530

    [vaishnavi] added: common layer

---
+++ CommonLayer/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class RegisterModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
