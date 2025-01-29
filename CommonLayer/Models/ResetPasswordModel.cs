3371fcc93e20798ab8c8732325cfe16055ae0f09
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-29 17:20:15 +0530

    [vaishnavi] added: reset password model

---
+++ CommonLayer/Models/ResetPasswordModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class ResetPasswordModel
+    {
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
