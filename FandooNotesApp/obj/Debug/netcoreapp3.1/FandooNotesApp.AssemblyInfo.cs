de8ec01e117bde65cfdec14bc73600a783f1d93f
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-30 17:23:17 +0530

    [vaishnavi] added: modified

--- FandooNotesApp/obj/Debug/netcoreapp3.1/FandooNotesApp.AssemblyInfo.cs
+++ FandooNotesApp/obj/Debug/netcoreapp3.1/FandooNotesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FandooNotesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f89f82d4f6755b528148b183406ef8dcd9fddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2ee8a41298592914df7434fd64754348dd2456c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FandooNotesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FandooNotesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
