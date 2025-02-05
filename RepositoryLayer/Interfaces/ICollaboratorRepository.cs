a9727fd6dce50f1feee3d5d80ce41fe22e037d76
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:47:03 +0530

    [vaishnavi] added: icollaborators repository

---
+++ RepositoryLayer/Interfaces/ICollaboratorRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface ICollaboratorRepository
+    {
+        public Collaborator AddCollaborator(AddCollaboratorModel model, int userId);
+        public List<Collaborator> GetAllCollaborators(int userId);
+        public bool DeleteCollaborator(string email, int notesId, int userId);
+    }
+}
