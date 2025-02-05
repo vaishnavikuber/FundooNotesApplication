2cb06eda8763ce2b405f806ec423fc419be5933e
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:47:22 +0530

    [vaishnavi] added: ilabel repository

---
+++ RepositoryLayer/Interfaces/ILabelRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface ILabelRepository
+    {
+        public Label AddLabel(LabelModel model, int userId);
+        public List<Label> GetAllLabels(int userId);
+        public bool DeleteLabel(int userId, int labelId);
+        public bool UpdateLabel(int userId, int labelId, string labelName);
+    }
+}
