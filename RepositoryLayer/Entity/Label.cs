2a4b977d405fad915671d3f0cafafe94677e2895
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:49:07 +0530

    [vaishnavi] added: label table

---
+++ RepositoryLayer/Entity/Label.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RepositoryLayer.Entity
+{
+    public class Label
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LabelID { get; set; }
+        public string LabelName { get; set; }
+
+        [ForeignKey("LabelNote")]
+        public int NotesId { get; set; }
+
+        [ForeignKey("LabelUser")]
+        public int UserID { get; set; }
+
+        [JsonIgnore]
+        public virtual Notes LabelNote { get; set; }
+
+        [JsonIgnore]
+        public virtual Users LabelUser { get; set; }
+
+    }
+}
