38de097c4622654c6a04caf8704e55cf0be7b682
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:55:40 +0530

    [vaishnavi] added: controller class

---
+++ RepositoryLayer/Entity/Collaborator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Text.Json.Serialization;
+
+
+namespace RepositoryLayer.Entity
+{
+    public class Collaborator
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CollaboratorId { get; set; }
+        public string Email { get; set; }
+        [ForeignKey("CollaboratorNotes")]
+        public int NotesID { get; set; }
+        [ForeignKey("CollaboratorUsers")]
+        public int UserId { get; set; }
+        [JsonIgnore]
+        public virtual Notes CollaboratorNotes { get; set; }
+        [JsonIgnore]
+        public virtual Users CollaboratorUsers { get; set; }
+    }
+}
