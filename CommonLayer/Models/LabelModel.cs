0663fd27b081ef6e825517c3f7c2ae10c569e998
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:51:51 +0530

    [vaishnavi] added: label model to add

---
+++ CommonLayer/Models/LabelModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class LabelModel
+    {
+        [Required(ErrorMessage ="without name label cannot be created")]
+        public string LabelName { get; set; }
+        [Required(ErrorMessage ="notes id required")]
+        public int NotesID { get; set; }
+    }
+}
