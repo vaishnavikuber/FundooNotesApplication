626d4912c26b231566b1d2becd4d4e199ca52b35
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:50:06 +0530

    [vaishnavi] added: icollaborator manager

---
+++ ManagerLayer/Interfaces/ICollaboratorManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace ManagerLayer.Interfaces
+{
+    public interface ICollaboratorManager
+    {
+        public Collaborator AddCollaborator(AddCollaboratorModel model, int userId);
+        public bool IsEmailExist(string email);
+        public List<Collaborator> GetAllCollaborators(int userId);
+        public bool DeleteCollaborator(string email, int notesId, int userId);
+    }
+}
