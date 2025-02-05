d1fe5cd57895b016eb444696f95092837aa99aa4
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:55:10 +0530

    [vaishnavi] added: ilabel manager

---
+++ ManagerLayer/Interfaces/ILabelManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace ManagerLayer.Interfaces
+{
+    public interface ILabelManager
+    {
+        public Label AddLabel(LabelModel model, int userId);
+        public List<Label> GetAllLabels(int userId);
+        public bool DeleteLabel(int userId,  int labelId);
+        public bool UpdateLabel(int userId, int labelId, string labelName);
+    }
+}
