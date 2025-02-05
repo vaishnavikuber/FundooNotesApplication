7cdfc3bd46b04ac2a7252d9e642202e6f8d6f609
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:48:02 +0530

    [vaishnavi] added: label manager

---
+++ ManagerLayer/Services/LabelManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+
+namespace ManagerLayer.Services
+{
+    public class LabelManager:ILabelManager
+    {
+        private readonly ILabelRepository label;
+
+        public LabelManager(ILabelRepository label)
+        {
+            this.label = label;
+        }
+        public Label AddLabel(LabelModel model, int userId)
+        {
+            return label.AddLabel(model, userId);
+        }
+        public List<Label> GetAllLabels(int userId)
+        {
+            return label.GetAllLabels(userId);
+        }
+        public bool DeleteLabel(int userId, int labelId)
+        {
+            return label.DeleteLabel(userId,  labelId);
+        }
+        public bool UpdateLabel(int userId, int labelId, string labelName)
+        {
+            return label.UpdateLabel(userId, labelId, labelName);
+        }
+    }
+}
