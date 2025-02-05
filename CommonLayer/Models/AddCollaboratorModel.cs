ca35a11f3aba759bd0b0432489681af2b476c84f
vaishnavi <vaishnavikuber2712@gmail.com>
2025-02-05 12:51:30 +0530

    [vaishnavi] added: collaborator model to add

---
+++ CommonLayer/Models/AddCollaboratorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class AddCollaboratorModel
+    {
+        [Required(ErrorMessage ="email required")]
+        [EmailAddress(ErrorMessage ="enter valid email")]
+        public string Email {  get; set; }
+        [Required(ErrorMessage ="notes id is required")]
+        public int NotesID { get; set; }
+    }
+}
